ba55c65adb3cb1ecd98ba0016b9ea5aaef029461
davidquintanajob <davidquintanajob@gmail.com>
2026-02-07 10:49:10 -0500

    Test

--- .output/server/chunks/build/index-styles.B0KBSSvP.mjs
+++
@@ -1,8 +0,0 @@
-const index_vue_vue_type_style_index_0_lang = "html{scroll-behavior:smooth}";
-
-const indexStyles_B0KBSSvP = [
-  index_vue_vue_type_style_index_0_lang
-];
-
-export { indexStyles_B0KBSSvP as default };
-//# sourceMappingURL=index-styles.B0KBSSvP.mjs.map
--- .output/server/chunks/build/leaflet-styles.CBCBBjsT.mjs
+++
@@ -1,8 +0,0 @@
-import { l as leaflet } from './entry-styles-2.mjs-D27W6EA7.mjs';
-
-const leafletStyles_CBCBBjsT = [
-  leaflet
-];
-
-export { leafletStyles_CBCBBjsT as default };
-//# sourceMappingURL=leaflet-styles.CBCBBjsT.mjs.map
